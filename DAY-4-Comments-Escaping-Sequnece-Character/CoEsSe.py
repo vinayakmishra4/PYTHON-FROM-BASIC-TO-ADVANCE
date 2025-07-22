cf35dae9505df1baf239f7f1349ef6814adc9d34
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-07-22 12:50:05 +0530

    Create CoEsSe.py

---
+++ DAY-4-Comments-Escaping-Sequnece-Character/CoEsSe.py
@@ -0,0 +1,20 @@
+# This sytanx of comments in python
+# print is function to give output to the console
+print("Day-4")
+# Escape Sequence Characters
+
+print("1. Backslash (\\):")
+print("   Path example: C:\\Users\\Admin\\Documents")
+
+print("\n2. Horizontal Tab (\\t):")
+print("   Name:\tJohn")
+print("   Age:\t\t25")
+
+print("\n3. Newline (\\n):")
+print("   Hello,\n   World!")
+
+print("\n4. Single Quote (\\'):")
+print('   It\'s a beautiful day.')
+
+print("\n5. Double Quote (\\\"):")
+print("   She said, \"Hello, World!\"")
